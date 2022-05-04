341b7b6f1376d3acb60f84cfae89f7631b377d5b
Ardiansyah Akbar <akbarardiansyah256@gmail.com>
2022-05-04 15:02:13 +0700

    repair some code

--- src/script.js
+++ src/script.js
@@ -8,30 +8,25 @@ document.onreadystatechange = () => {
      </div>\
  </div>\
  <nav class="container">\
-     <div id="nav-brand"><a href="/"> <img src="https://www.freepnglogos.com/uploads/javascript-png/javascript-vector-logo-yellow-png-transparent-javascript-vector-12.png" width="50"> </a>\
+     <div id="nav-brand"><a href="/"> <img src="assets/javascript.png" width="50"> </a>\
          <form class="column" action="/kelas" method="GET">\
              <input type="search" class="input search" name="query" placeholder="mau belajar apa?">\
          </form>\
      </div>\
      <div id="nav-menu">\
          <div class="nav-item">\
-             <a href="/kelas-feed" class="nav-item">Belajar</a>\
+             <a href="index.html" class="nav-item">Belajar</a>\
              <div class="nav-sub-menu">\
+             <a href="#" class="nav-sub-menu-item">Variabel dan Konstanta</a>\
                  <a href="function.html" class="nav-sub-menu-item">function</a>\
-                 <a href="/panduan" class="nav-sub-menu-item">Panduan</a>\
-                 <a href="/track" class="nav-sub-menu-item">Perjalanan</a>\
+                 <a href="#" class="nav-sub-menu-item">Tipe data</a>\
+                 <a href="#" class="nav-sub-menu-item">Operator</a>\
              </div>\
          </div>\
          <div class="nav-item">\
-             <a href="/nongkrong" class="nav-item">Nongkrong</a>\
-             <div class="nav-sub-menu">\
-                 <a href="/nongkrong" class="nav-sub-menu-item">Semua</a>\
-                 <a href="/forum" class="nav-sub-menu-item">Forum</a>\
-                 <a href="/artikel" class="nav-sub-menu-item">Artikel</a>\
-                 <a href="/mading" class="nav-sub-menu-item">Mading</a>\
-             </div>\
+             <a href="mislecenaus.html" class="nav-item">mislecenaus</a>\
          </div>\
-         <a href="/login" class="nav-item">Login</a>\
+         <a href="#" class="nav-item">#</a>\
      </div>\
      <div class="nav-toggle" onclick="toggleMenu(this)">\
          <div class="bar1"></div>\
